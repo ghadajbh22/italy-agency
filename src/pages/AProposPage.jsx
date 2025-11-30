0013b565384b76501d17621c4a4068c30fa22640
Ghada <ghadaajbh@gmail.com>
2025-11-30 15:24:55 +0100

    feat: Add Docker configuration, Jenkins pipelines, and fix React imports

--- src/pages/AProposPage.jsx
+++ src/pages/AProposPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import APropos from '../components/APropos';
-import Footer from '../components/Footer';
+
 
 function AProposPage() {
     return (
         <div>
             <Header />
             <APropos />
-            <Footer />
+           
         </div>
     );
 }
